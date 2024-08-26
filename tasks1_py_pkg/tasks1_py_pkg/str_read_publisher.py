d2ead776414fbc76103cc9dbd0a221a6eaabedeb
ZisanNurYKSL <zisannuryuksel2323@gmail.com>
2024-08-26 10:31:59 +0300

    enhance(publisher): Terminal girişine izin verilerek iyileştirilmiş kullanıcı deneyimi

--- tasks1_py_pkg/tasks1_py_pkg/str_read_publisher.py
+++ tasks1_py_pkg/tasks1_py_pkg/str_read_publisher.py
@@ -9,27 +9,23 @@ class StrReadPublisher(Node):# 1 #Node'u buraya girdiğimde aslında bu class ge
     def __init__(self):
         super().__init__("str_read_publisher") #2
 
-        self.publisher_ = self.create_publisher(String, "/terminal_str", 10 )
+        self.publisher_ = self.create_publisher(String, "/terminal_str", 10 ) 
+        self.get_logger().info("Talker Python node has been started")  
+
+        while rclpy.ok():
+            msg = String()
+            input_user = input("Lütfen String Giriniz:")
+            msg.data = input_user
+            self.publisher_.publish(msg)
         
-        timer_period = 0.5
-        self.timer = self.create_timer(timer_period, self.publisher_str_read)
-        self.i = 0 #sayac
-        
-
-
-    def publisher_str_read(self):
-        msg = String()
-        input_user = input("Lütfen String Giriniz:")
-        msg.data = input_user
-        self.publisher_.publish(msg)
-
 
 # 2. Metodu yaz
 def main(args=None):
     rclpy.init(args=args) #bir metodu başlatmak için yapmam gereken şey
     node = StrReadPublisher() # 3 #node'u oluşturmak için
     rclpy.spin(node)#bu düğümün yaşam döngüsünü başlatırım
-    node.destroy_node() #BUNU NEDEN YAPTIM
+    #aşağıdaki iki satır da node'un yok edilme aşamalarıdır
+    node.destroy_node() 
     rclpy.shutdown() # ben düğümü kapattığımda veya durdurduğumda shutdown'a gelecek ve düğümü yok edecek
 
 
@@ -38,3 +34,4 @@ def main(args=None):
 
 if __name__ == "__main__": 
     main()
+    
