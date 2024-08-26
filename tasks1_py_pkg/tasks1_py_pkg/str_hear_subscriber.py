24620ce4b0c38a4e6cc45a1f77c7eba0910ba572
ZisanNurYKSL <zisannuryuksel2323@gmail.com>
2024-08-26 10:34:33 +0300

    feat(subscriber): Subscriber düğümü oluşturuldu

--- tasks1_py_pkg/tasks1_py_pkg/str_hear_subscriber.py
+++ tasks1_py_pkg/tasks1_py_pkg/str_hear_subscriber.py
@@ -2,20 +2,25 @@
 
 import rclpy
 from rclpy.node import Node
-from example_interfaces.msg import String
+from std_msgs.msg import String
+import time
 
 class StrHearSubscriber(Node):
     def __init__(self):
         super().__init__("str_hear_subscriber")
         self.subscriber_ = self.create_subscription(String, "/terminal_str", self.callback_str_read, 10)
 
-        self.get_logger().info("Started listening to the terminal")
+        self.get_logger().info("Listener Python node has been started")
+
+        self.subscriber_ #kullanılmayan değişken uyarısını engelle
+
 
     
     def callback_str_read(self,msg):
-        self.get_logger().info(msg.data)
-
-
+        now = time.time()
+        current_time = time.localtime(now)
+        formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", current_time)
+        self.get_logger().info('I heard: "%s" - Time: %s' % (msg.data, formatted_time))
 
 
 
@@ -23,6 +28,7 @@ def main(args=None):
     rclpy.init(args=args)
     node = StrHearSubscriber()
     rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown
 
 
