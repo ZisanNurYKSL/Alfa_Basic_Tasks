45ef5f3fe29e57b4828069b4fe0b369fa4936c37
Zisan Nur Yuksel <zisannuryuksel2323@gmail.com>
2024-09-18 16:31:02 +0300

    feat: Publisher and subscriber apps created

---
+++ src/tasks1_py_pkg/tasks1_py_pkg/weather_forecast_pub.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import rclpy
+from rclpy.node import Node
+from char_to_string_interfaces_pkg.msg import WeatherForecast
+import random
+import time
+
+class WeatherForecastPublisher(Node):
+    def __init__(self):
+        super().__init__('weather_publisher')
+        self.publisher_ = self.create_publisher(WeatherForecast, 'weather_data', 10)
+        self.timer = self.create_timer(5.0, self.publish_weather_data)
+
+    def publish_weather_data(self):
+        msg = WeatherForecast()
+        msg.temperature = random.uniform(-10.0, 40.0)  # Sıcaklık
+        msg.humidity = random.uniform(0.0, 100.0)      # Nem
+        msg.wind_speed = random.uniform(0.0, 20.0)     # Rüzgar hızı       
+        
+        self.publisher_.publish(msg) #??
+
+        self.get_logger().info(f'Publishing: Temperature: {msg.temperature:.2f} °C,, Humidity: {msg.humidity:.2f} %, Wind Speed: {msg.wind_speed:.2f} km/h')
+
+def main(args=None):
+    rclpy.init(args=args)
+    weather_publisher = WeatherForecastPublisher()
+    rclpy.spin(weather_publisher)
+    weather_publisher.destroy_node() 
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ src/tasks1_py_pkg/tasks1_py_pkg/weather_forecast_sub.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import rclpy
+from rclpy.node import Node
+from char_to_string_interfaces_pkg.msg import WeatherForecast
+import time
+
+class WeatherForecastSubscriber(Node):
+    def __init__(self):
+        super().__init__("weather_forecast_sub")
+
+        self.subscriber_ = self.create_subscription(WeatherForecast, "/weather_data", self.callback_weather_forecast_read, 10)
+
+        self.get_logger().info("Listener Python node has been started")
+
+        self.subscriber_ #kullanılmayan değişken uyarısını engelle
+
+
+    
+    def callback_weather_forecast_read(self,msg):
+        """
+        Used to process incoming weather data at the ROS2 Subscriber node
+        """
+        temperature = msg.temperature
+        humidity = msg.humidity
+        wind_speed = msg.wind_speed   
+
+        self.get_logger().info(f'Received data - Temperature: {temperature:.2f} °C, Humidity: {humidity:.2f} %, Wind Speed: {wind_speed:.2f} km/h')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    weather_subscriber = WeatherForecastSubscriber()
+    rclpy.spin(weather_subscriber)
+    weather_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
