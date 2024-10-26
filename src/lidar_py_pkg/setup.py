1522399a53d6af746a807f7fd360b3fc71a67819
Zisan Nur Yuksel <zisannuryuksel2323@gmail.com>
2024-10-26 11:10:07 +0300

    feat(package): updated setup.py. added a new node named lidar_data_node to the package.

---
+++ src/lidar_py_pkg/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'lidar_py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='zisannur',
+    maintainer_email='zisannuryuksel2323@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "lidar_data_node = lidar_py_pkg.lidar_data_node:main"
+
+        ],
+    },
+)
